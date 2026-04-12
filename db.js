8776eb4edf2ab5dbcf672b7292a80168c261a2ad
animeshpramanik719-netizen <animeshpramanik719@gmail.com>
2026-04-12 16:12:47 +0530

    next version of project

--- db.js
+++ db.js
@@ -3,8 +3,24 @@ const mongoose = require("mongoose");
 // Define the mongodb connection URL
 // FIXED: Changed the port from 2701 to the standard 27017
 //const mongoURL = "mongodb://localhost:27017/hotels"
-const mongoURL = 'mongodb+srv://animesh9906:animesh990678@cluster0.oubyoyx.mongodb.net/';
-mongoose.connect(mongoURL);
+const atlasURL = 'mongodb+srv://animesh9906:animesh990678@cluster0.oubyoyx.mongodb.net/';
+const localURL = 'mongodb://127.0.0.1:27017/hotels';
+const connectDB = async () => {
+  try {
+    await mongoose.connect(atlasURL);
+    console.log('Connected to MongsoDB Atlas');
+  } catch (err) {
+    console.log('Atlas failed, connecting to Local DB...');
+    
+    try {
+      await mongoose.connect(localURL);
+      console.log('Connected to Local MongoDB');
+    } catch (localErr) {
+      console.log('Local DB also failed:', localErr);
+      process.exit(1);
+    }
+  }
+};
 
 // Connect to the MongoDB database using Mongoose
 // Get the default connection
@@ -14,4 +30,4 @@ const db = mongoose.connection;
 
 
 // Export the database connection
-module.exports = db;
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express'); // FIXED: Changed import to require
 const app = express();
-const db = require('./db'); 
+const connectDB = require('./db'); 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body // download body parser npm 
 const mongoose= require('mongoose');
+const Person = require('./person');
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Welcome to my hotel... How can i help you ?');
@@ -15,7 +18,7 @@ app.post('/person', async (req, res) =>{
     const data= req.body
   
   //create a new person document using the person model
-  const newPerson = new person(data);
+  const newPerson = new Person(data);
  
  //save the new person document to the database  
  const response = await newPerson.save();
@@ -34,10 +37,12 @@ const personRouter = require('./personRoutes');
 const menuItemRoutes = require('./MenuItemsRoutes');
 //use the router for all routes starting with /person
 app.use('/person', personRouter); 
-//use the router for all routes starting with /menuItem
+//use the router for all routnodees starting with /menuItem
 app.use('/menuItem', menuItemRoutes); 
 
 // start the server
-app.listen(6969, () => {
-  console.log('Server is running on http://localhost:6969');
+const PORT = process.env.PORT || 6969;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
