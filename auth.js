5308c22ffa90b76e818004a5a5b3f9b60044b3ef
animeshpramanik719-netizen <animeshpramanik719@gmail.com>
2026-04-18 19:23:05 +0530

    new add

--- auth.js
+++ auth.js
@@ -1,31 +1,31 @@
-const passport=require('passport');
-const LocalStrategy= require('passport-local').Strategy;
-const person=require('./person');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Person = require('./person');
 
- //Middleware function  
- const logrequest=(req, res, next)=>{
-    console.log(`${new Date().toLocaleString()} Request Mode to: ${req.originalUrl}`);
-    next(); // Call the next middleware function
- }
+// Local Strategy Authentication
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      console.log('Received username:', username);
 
-app.use(logrequest);//use the middleware function
+      const user = await Person.findOne({ username: username });
 
-passport.use(new LocalStrategy(async(username, password, done) =>{
-  //authenticate the user using the username and password
-  try{
-    console.log('Received username:', username, password);
-    const user= await person.findOne({username: username});
-    if(!user)
-      return done(null, false,{message: 'Incorrect username '});
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
 
-const isPasswordMatch=user.password === password ? true : false;
-if(!isPasswordMatch){
-  return done(null, false);
-}else{
-  return done(null, false, {message: 'Incorrect password'});
-}
-  }catch(err){
-return done(err);
-  }
-}))
-module.exports= passport; // Export configured passport
+      const isPasswordMatch = user.password === password;
+
+      if (isPasswordMatch) {
+        return done(null, user); // Login success
+      } else {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+module.exports = passport;
--- server.js
+++ server.js
@@ -1,62 +1,63 @@
-const express = require("express"); // FIXED: Changed import to require
+const express = require("express");
 const app = express();
+
 const connectDB = require("./db");
-const bodyParser = require("body-parser");
-app.use(bodyParser.json()); //req.body // download body parser npm
-const mongoose = require("mongoose");
-const Person = require("./person");
-const passport= require('./auth')
 require("dotenv").config();
 
+const bodyParser = require("body-parser");
+const Person = require("./person");
+const passport = require("./auth");
 
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(bodyParser.json());
 app.use(passport.initialize());
 
- const logrequest=(req, res, next)=>{
-    console.log(`${new Date().toLocaleString()} Request Mode to: ${req.originalUrl}`);
-    next(); // Call the next middleware function
- }
+// Logger Middleware
+const logrequest = (req, res, next) => {
+  console.log(`${new Date().toLocaleString()} Request Made to: ${req.originalUrl}`);
+  next();
+};
 
-app.use(logrequest);//use the middleware function
+app.use(logrequest);
 
- 
-const localAuthMiddleware = passport.authenticate('local', {session: false})
-app.get("/", localAuthMiddleware, function(req, res) {
-  res.send("Welcome to my hotel... How can i help you ?");
+// Auth Middleware
+const localAuthMiddleware = passport.authenticate('local', { session: false });
+
+// Home Route (Protected)
+app.get("/", localAuthMiddleware, (req, res) => {
+  res.send("Welcome to my hotel... How can I help you?");
 });
 
-//POST route to create a new person
-app.post('/person', async (req, res) =>{
-   try{
-     const data= req.body
+// POST Route Create Person
+app.post("/person", async (req, res) => {
+  try {
+    const data = req.body;
 
-//   //create a new person document using the person model
-   const newPerson = new Person(data);
+    const newPerson = new Person(data);
 
-//  //save the new person document to the database
-  const response = await newPerson.save();
-  console.log('data saved');
-  res.status(200).json(response)
+    const response = await newPerson.save();
 
-  }catch(err){
+    console.log("Data Saved");
+
+    res.status(200).json(response);
+
+  } catch (err) {
     console.log(err);
-     res.status(500).json({error: 'internal server error'});
-   }
- });
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-
-
-app.use(logrequest); // Use the middleware function
-
-
-// Import the router file
+// Routers
 const personRouter = require("./personRoutes");
 const menuItemRoutes = require("./MenuItemsRoutes");
-//use the router for all routes starting with /person
+
 app.use("/person", personRouter);
-//use the router for all routnodees starting with /menuItem
 app.use("/menuItem", menuItemRoutes);
 
-// start the server
+// Server Start
 const PORT = process.env.PORT || 6969;
 
 app.listen(PORT, () => {
